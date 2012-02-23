73aabd9c273938c9e9af620b51a8467567be4093
J. Venator <j@venator.vc>
2012-02-23 15:14:51 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wallet::Application.config.secret_token = '39e4095cb3df3a217e47f4bafb2f39303284863e1d413da7bbd74d350ea6b6b7f6157b81eeee167079fa40ff30309795042bbead2bc6628cbf123c4dc47bcdbb'
