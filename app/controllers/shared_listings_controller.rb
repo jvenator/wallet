bd67a9744cc96ce9c3692d7c6c77877da9eea31f
Drew Purdy <andrewmp1@gmail.com>
2012-04-17 12:48:27 -0700

    Fixing packages not showing when no packages exist for shared_listing

--- app/controllers/shared_listings_controller.rb
+++ app/controllers/shared_listings_controller.rb
@@ -1,10 +1,14 @@
 class SharedListingsController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def show
     @shared_listing = current_user.shared_listings.find(params[:id])
     @listing_user = @shared_listing.listing.user
     @documents = @shared_listing.listing.documents
-    @packages = current_user.packages.where(["id NOT IN (?)", @shared_listing.package_ids]).order("name asc")
+    if @shared_listing.packages
+      @packages = current_user.packages.where(["id NOT IN (?)", @shared_listing.package_ids]).order("name asc")
+    else
+      @packages = current_user.packages
+    end
   end
 end
