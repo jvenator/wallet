184eec231073f8f7b599b57def9df385c4ac5ed5
Andrew Purdy <andrewmp1@gmail.com>
2012-05-11 17:33:21 -0700

    small fix to redirect admin to /admin/users/ url on login

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,13 +7,14 @@ class DashboardsController < ApplicationController
     @shared_packages = current_user.shared_packages.where(:receiver_email => current_user.email)
     @listings = current_user.listings
     @shared_listings = current_user.shared_listings.where(:receiver_email => current_user.email)
-    
-    if current_user.renter?
-      render "dashboard_renter"
+
+
+    if current_user.admin?
+      redirect_to admin_users_path
     elsif current_user.owner_rep?
       render "dashboard_owner_rep"
     else
-      render "dashboard_admin"
+      render "dashboard_renter"
     end
   end
 end
