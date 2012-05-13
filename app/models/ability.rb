9bd69a882c80496392bf3b595f041eaf4504db72
Andrew Purdy <andrewmp1@gmail.com>
2012-05-13 14:29:24 -0700

    Adding in a few tests to Listing model.  Rough cut on abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,29 +2,46 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   elsif user.manager?
-    #      can :manage, Listing
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    user ||= User.new # guest user (not logged in)
+
+    if user.has_role? :admin
+      can :manage, :all
+    elsif user.has_role? :manager
+      can :create, Listing
+      can :update, Listing do |listing|
+        listing.try(:user) == user
+      end
+      can :create, Document
+      can :update, Document do |document|
+        document.try(:user) == user
+      end
+
+    elsif user.has_role? :broker
+      can :create, Listing
+      can :update, Listing do |listing|
+        listing.try(:user) == user
+      end
+      can :create, Document
+      can :update, Document do |document|
+        document.try(:user) == user
+      end
+
+    elsif user.has_role? :renter
+      can :create, Document
+      can :update, Document do |document|
+        document.try(:user) == user
+      end
+      can :create, Package
+      can :update, Package do |package|
+        package.try(:user) == user
+      end
+      can :create, RenterProfile
+      can :update, RenterProfile do |profile|
+        profile.try(:user) == user
+      end
+
+    else
+      can :read, :all
+    end
   end
 end
