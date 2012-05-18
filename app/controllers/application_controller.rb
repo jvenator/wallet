9328a2bef1a971138e94d2e5f6fc00ec16a2d70b
J. Venator <j@venator.vc>
2012-05-18 15:30:51 -0700

    Create renter layout and implemented refined primary nav bar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  layout :choose_layout
   #rescue_from CanCan::AccessDenied do |exception|
   #  redirect_to root_url, :alert => exception.message
   #end
@@ -11,4 +11,19 @@ class ApplicationController < ActionController::Base
     authenticate_user!
     redirect_to root_path, alert: "You must be an admin to do that." unless current_user.admin?
   end
+
+  def choose_layout
+  	if current_user
+      if !current_user.renter?
+    		'application'
+    	else
+    		'renter'
+    	end
+    else
+      'application'
+    end
+  end
+  		
+
+
 end
