ff6697b8045622aed7c6f27ad82d68798eca155a
Andrew Purdy <andrewmp1@gmail.com>
2012-05-18 16:58:48 -0700

    configuring s3 bucket to be set in your services.yml or environment

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,6 +3,7 @@ if !Rails.env.production?
   config = YAML.load_file(Rails.root.join("config", "services.yml"))
   ENV['S3_KEY'] = config["s3"]["key"]
   ENV['S3_SECRET'] = config["s3"]["secret"]
+  ENV['S3_BUCKET'] = config["s3"]["bucket"]
 end
 
 CarrierWave.configure do |config|
@@ -12,7 +13,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['S3_SECRET']    # required
     # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'renters-wallet-documents'                     # required
+  config.fog_directory  = ENV['S3_BUCKET']                     # required
   
   config.cache_dir = "#{Rails.root}/tmp/uploads"    # 
   # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
